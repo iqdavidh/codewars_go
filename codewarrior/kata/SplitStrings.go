de03dfbc7d7960a1c0d3cbeb87e8894e37dbe58a
davidh <iq.davidh@gmail.com>
2020-04-26 22:09:21 -0500

    inicio

---
+++ codewarrior/kata/SplitStrings.go
@@ -0,0 +1,15 @@
+package kata
+
+//Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//
+//Examples:
+//
+//Solution("abc") //should return ["ab", "c_"]
+//Solution("abcdef") //should return ["ab", "cd", "ef"]
+//
+
+func Solution(str string) []string {
+
+	var respuesta = []string{"nada"}
+	return respuesta
+}
