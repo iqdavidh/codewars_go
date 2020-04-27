4103218959246d10e119992aa6a251402a49f86f
davidh <iq.davidh@gmail.com>
2020-04-26 22:46:14 -0500

    testing funcioanndo

--- codewarrior/kata/SplitStrings.go
+++ codewarrior/kata/SplitStrings.go
@@ -1,6 +1,9 @@
 package kata
 
-//Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//Complete the solution so that it splits the string
+//into pairs of two characters. If the string contains
+//an odd number of characters then it should replace the
+//missing second character of the final pair with an underscore ('_').
 //
 //Examples:
 //
@@ -8,8 +11,21 @@ package kata
 //Solution("abcdef") //should return ["ab", "cd", "ef"]
 //
 
-func Solution(str string) []string {
+func Solution(texto string) []string {
 
-	var respuesta = []string{"nada"}
+	size := len(texto)
+	if size%2 > 0 {
+		size++
+		texto += "_"
+	}
+
+	contador := size / 2
+
+	var respuesta = make([]string, contador)
+
+	for i := 0; i < contador; i++ {
+		respuesta[i] = texto[i*2 : i*2+2]
+	}
+
 	return respuesta
 }
